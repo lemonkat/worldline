f25d7f388a094a65d37275f72cf2a41a75af4065
Lemon Kat <lemonkatto@gmail.com>
2026-07-27 10:42:48 -0700

    added __all__ to __init__.py

--- worldline/__init__.py
+++ worldline/__init__.py
@@ -1,6 +1,30 @@
-from worldline import uid, data, llm, worldline
+from worldline import uid, data, llm, worldline, library
 from worldline.uid import UID, UIDGenerator
 from worldline.data import PageCounter, Config, Context, Note
 from worldline.llm import importance, init, get_importance, get_emb, count_sentences
 from worldline.worldline import Worldline
 from worldline.library import MissingRecordError, UnloadedRecordError, Record, Library
+
+__all__ = [
+    "uid",
+    "data",
+    "llm",
+    "worldline",
+    "library",
+    "UID",
+    "UIDGenerator",
+    "PageCounter",
+    "Config",
+    "Context",
+    "Note",
+    "importance",
+    "init",
+    "get_importance",
+    "get_emb",
+    "count_sentences",
+    "Worldline",
+    "MissingRecordError",
+    "UnloadedRecordError",
+    "Record",
+    "Library",
+]
