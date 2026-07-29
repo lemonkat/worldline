beb71f80d071ff5245b48debfd3e408fd5ede8cf
Lemon Kat <lemonkatto@gmail.com>
2026-07-29 12:21:53 -0700

    updated __init__.py

--- worldline/__init__.py
+++ worldline/__init__.py
@@ -1,9 +1,10 @@
-from worldline import uid, data, llm, worldline, library
+from worldline import uid, data, llm, worldline, library, agent
 from worldline.uid import UID, UIDGenerator
 from worldline.data import PageCounter, Config, Context, Note
 from worldline.llm import importance, init, get_importance, get_emb, count_sentences
 from worldline.worldline import Worldline
 from worldline.library import MissingRecordError, UnloadedRecordError, Record, Library
+from worldline.agent import tool_d20, WorldlineAgent
 
 __all__ = [
     "uid",
@@ -27,4 +28,6 @@ __all__ = [
     "UnloadedRecordError",
     "Record",
     "Library",
+    "tool_d20",
+    "WorldlineAgent",
 ]
